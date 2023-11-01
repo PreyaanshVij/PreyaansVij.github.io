9a80c06ad2daf431af6b7a90f70cebadb45d6f14
PreyaanshVij <147871101+PreyaanshVij@users.noreply.github.com>
2023-11-01 18:34:04 +0530

    Update scartch.js

--- scartch.js
+++ scartch.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
     var password = document.querySelector('input[name="password"]').value;
     var errorMessage = document.getElementById('error-message');
     
-    if (username === 'preyaansh' && password === 'niit') {
+    if (username === 'Preyaansh Vij' && password === 'BT23GCS168') {
         window.location.href = 'dashboard.html';
     } else {
         errorMessage.textContent = 'Try again';
