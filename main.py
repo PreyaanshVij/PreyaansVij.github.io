de4c5efed639577d1ea1e98cbfd000d1e2dd4a2a
PreyaanshVij <147871101+PreyaanshVij@users.noreply.github.com>
2024-02-01 22:48:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+a = input("In Which City You Were Born In? :  ")
+b = input("What Is The Name Of Your Pet? :  ")
+
+c = a.capitalize() + b.capitalize()
+
+print(f"The Name Of Your Band Could Be {c}.")
